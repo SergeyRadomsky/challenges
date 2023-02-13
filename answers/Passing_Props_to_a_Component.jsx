a59fc021607c3915eac59cd6e93696e73f45b656
Sergey Radomsky <radomskijs@mail.ru>
2023-02-13 18:26:29 +0300

    Passing_Props_to_a_Component

--- answers/Passing_Props_to_a_Component.jsx
+++ answers/Passing_Props_to_a_Component.jsx
@@ -4,7 +4,7 @@ import { getImageUrl } from './utils.js';
 const Mary = {
   name: "Maria Skłodowska-Curie",
   profession: "physicist and chemist",
-  img:{
+  img: {
     classname: "avatar",
     src: 'szV5sdG',
     alt: "Maria Skłodowska-Curie",
@@ -32,29 +32,29 @@ const Katusho = {
 
 const Section = (props) => {
   return (
-   <section className="profile">
-        <h2>{props.Name.name}</h2>
-        <img
-          className= "avatar"
-          src = {props.getImageUrl(props.Name.img.src)}
-          alt={props.Name.img.alt}
-          width={props.Name.img.width}
-          height={props.Name.img.width}
-        />
-        <ul>
-          <li>
-            <b>Profession: </b> 
-            {props.Name.profession}
-          </li>
-          <li>
-            <b>Awards: {props.Name.counterAwards}</b> 
-            {props.Name.AwardsText}
-          </li>
-          <li>
-            <b>Discovered: </b>
-            {props.Name.Discovered}
-          </li>
-        </ul>
+    <section className="profile">
+      <h2>{props.Name.name}</h2>
+      <img
+        className="avatar"
+        src={props.getImageUrl(props.Name.img.src)}
+        alt={props.Name.img.alt}
+        width={props.Name.img.width}
+        height={props.Name.img.width}
+      />
+      <ul>
+        <li>
+          <b>Profession: </b>
+          {props.Name.profession}
+        </li>
+        <li>
+          <b>Awards: {props.Name.counterAwards}</b>
+          {props.Name.AwardsText}
+        </li>
+        <li>
+          <b>Discovered: </b>
+          {props.Name.Discovered}
+        </li>
+      </ul>
     </section>
   )
 }
@@ -63,12 +63,12 @@ export default function Gallery() {
   return (
     <div>
       <h1>Notable Scientists</h1>
-      <Section 
+      <Section
         Name={Mary}
         img={Mary.img}
         getImageUrl={getImageUrl}
       />
-      <Section 
+      <Section
         Name={Katusho}
         img={Katusho.img}
         getImageUrl={getImageUrl}
@@ -85,7 +85,7 @@ function Avatar(props) {
     if (props.size < 90) return "s"
     else return ("b")
   }
-  
+
   return (
     <img
       className="avatar"
@@ -101,8 +101,8 @@ export default function Profile() {
   return (
     <Avatar
       size={90}
-      person={{ 
-        name: 'Gregorio Y. Zara', 
+      person={{
+        name: 'Gregorio Y. Zara',
         imageId: '7vQD0fP'
       }}
     />
@@ -112,38 +112,49 @@ export default function Profile() {
 //5.3
 export default function Profile() {
 
-  const Card1 = () => {
+
+
+  const Card0 = ({ children }) => {
     return (
-            <div className="card">
+      <div className="card">
         <div className="card-content">
-          <h1>Photo</h1>
-          <img
-            className="avatar"
-            src="https://i.imgur.com/OKS67lhm.jpg"
-            alt="Aklilu Lemma"
-            width={70}
-            height={70}
-          />
+          {children}
         </div>
       </div>
     )
   }
 
-  const Card2 = () => {
-  return (
-          <div className="card">
-      <div className="card-content">
-        <h1>About</h1>
-        <p>Aklilu Lemma was a distinguished Ethiopian scientist who discovered a natural treatment to schistosomiasis.</p>
-      </div>
-    </div>
+  const Card1 = () => {
+    return (
+      <>
+        <h1>Photo</h1>
+        <img
+          className="avatar"
+          src="https://i.imgur.com/OKS67lhm.jpg"
+          alt="Aklilu Lemma"
+          width={70}
+          height={70}
+        />
+      </>
     )
   }
-  
+
+  const Card2 = () => {
+    return (
+      <>
+        <h1>About</h1>
+        <p>Aklilu Lemma was a distinguished Ethiopian scientist who discovered a natural treatment to schistosomiasis.</p>
+      </>
+    )
+  }
   return (
     <>
-    <Card1 />
-    <Card2 />
+      <Card0>
+        <Card1 />
+      </Card0>
+      <Card0>
+        <Card2 />
+      </Card0>
     </>
   );
 }
