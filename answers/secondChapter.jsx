c9bdd0e7b3dedcf81bae320cf413aac6b6828dd4
Sergey Radomsky <radomskijs@mail.ru>
2023-02-13 15:44:25 +0300

    secondChapter answer

---
+++ answers/secondChapter.jsx
@@ -0,0 +1,34 @@
+// 2.1
+//App.js
+import Gallery from './Gallery.js';
+import Profile from './Profile.js';
+
+export default function App() {
+  return (
+    <div>
+      <Profile />
+      <Gallery />
+    </div>
+  );
+}
+// Gallery.js
+import Profile from "./Profile.js"
+export default function Gallery() {
+  return (
+    <section>
+      <h1>Amazing scientists</h1>
+      <Profile />
+      <Profile />
+      <Profile />
+    </section>
+  );
+}
+//Profile.js
+export default function Profile() {
+  return (
+    <img
+      src="https://i.imgur.com/QIrZWGIs.jpg"
+      alt="Alan L. Hart"
+    />
+  );
+}
